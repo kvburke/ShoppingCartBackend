24e4836e798d20776e6ed74c4c3dfaaa3b2cdd43
kburke <kvburke96@gmail.com>
2018-09-24 11:57:23 -0400

    second commit

--- maven-project/src/main/java/com/boraji/tutorial/maven/HelloMaven.java
+++ maven-project/src/main/java/com/boraji/tutorial/maven/HelloMaven.java
@@ -6,6 +6,6 @@ import org.junit.Test;
 public class HelloMaven {
 	@Test
 	public void test() {
-		Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+		Assert.assertEquals("Hello Mavenx", new String("Hello Mavenx"));
 	}
 }
